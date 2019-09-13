37358221b6db04f88c2c8bfb6f57bd950e5abac7
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2019-09-12 20:57:30 -0700

    updated calculation of NextArrival and MinutesAway

--- app.js
+++ app.js
@@ -44,21 +44,24 @@ $(document).ready(function () {
     trainListRef.on("child_added", function (snapshot) {
 
         var trEle = $("<tr>");
+
+        // create <td> for the first 3 fields: Train Name, Destination, Frequency
         var trainTdEle = $("<td>" + snapshot.val().trainName + "</td>");
         var destinationTdEle = $("<td>" + snapshot.val().destination + "</td>");
         var frequencyTdEle = $("<td>" + snapshot.val().frequency + "</td>");
 
-
+        // calculate Next Arrival
         var now = moment();
         var nextArrival = moment(snapshot.val().firstTrainTime, "HH:mm");
 
         while (nextArrival.isBefore(now)) {
-            nextArrival.add(parseInt(snapshot.val().frequency), "m");
+            nextArrival.add(parseInt(snapshot.val().frequency), 'minutes');
         }
 
-        var minutesAway = nextArrival.diff(now, "minutes");
+        // calculate Minutes Away
+        var minutesAway = Math.round(nextArrival.diff(now, "minutes", true));
 
-
+        // create <td> for Next Arrival and Minutes Away
         var nextArrivalTdEle = $("<td>" + nextArrival.format("LT") + "</td>");
         var minutesAwayTdEle = $("<td>" + minutesAway + "</td>");
 
@@ -66,10 +69,70 @@ $(document).ready(function () {
 
         $("tbody").append(trEle);
 
-    },
+    }, function (errorObject) {
+        console.log("error: " + errorObject.code);
+    });
 
-        function (errorObject) {
-            console.log("error: " + errorObject.code);
+
+    /*
+    - function "redrawTable" is to remove elements in <tbody>, then get all the train data from firebase and re-create elements to display in <tbody>;
+    - then use setInterval to call this function every 1 minute so Next Arrival and Minutes Away are up to date;
+    - for now, I need to comment out these code as I don't want it keep querying firebase
+
+    function redrawTable() {
+
+        $("tbody").empty();
+
+        trainListRef.once('value', function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+
+                var trEle = $("<tr>");
+
+                // create <td> for the first 3 fields: Train Name, Destination, Frequency
+                var trainTdEle = $("<td>" + childSnapshot.val().trainName + "</td>");
+                var destinationTdEle = $("<td>" + childSnapshot.val().destination + "</td>");
+                var frequencyTdEle = $("<td>" + childSnapshot.val().frequency + "</td>");
+
+                // calculate Next Arrival
+                var now = moment();
+                var nextArrival = moment(childSnapshot.val().firstTrainTime, "HH:mm");
+
+                while (nextArrival.isBefore(now)) {
+                    nextArrival.add(parseInt(childSnapshot.val().frequency), 'minutes');
+                }
+
+                // calculate Minutes Away
+                var minutesAway = Math.round(nextArrival.diff(now, "minutes", true));
+
+                // create <td> for Next Arrival and Minutes Away
+                var nextArrivalTdEle = $("<td>" + nextArrival.format("LT") + "</td>");
+                var minutesAwayTdEle = $("<td>" + minutesAway + "</td>");
+
+                trEle.append(trainTdEle, destinationTdEle, frequencyTdEle, nextArrivalTdEle, minutesAwayTdEle);
+
+                $("tbody").append(trEle);
+
+            });
         });
+    }
+
+    setInterval(redrawTable, 60000);
+
+
+    // Another solution to calculate Next Arrival and Minutes Away:
+
+    var tFrequency = parseInt(snapshot.val().frequency);
+    var firstTime = snapshot.val().firstTrainTime;
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    var currentTime = moment();
+    var diffTime = moment().diff(firstTimeConverted, "minutes");
+    var tRemainder = diffTime % tFrequency;
+    var minutesAway = tFrequency - tRemainder;
+    var nextArrival = moment().add(minutesAway, "minutes");
+    */
 
 });
+
+
+
+
